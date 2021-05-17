e042fd01540ffbd5a516e0bd8f595ac4285ca90b
michaelshxw <michaelshxw@gmail.com>
2021-05-17 17:43:22 +0800

    add test files, add comments for todos

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,22 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = {
+    question1: "question1",
+    question2: "question2",
+    question3: "question3"
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    //function to write README.md here
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //function to initialise app here
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // function to render license badge
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // function to render license link
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // function to render license section
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  //function to generate markdown here
+  return `# ${data.title}`;
+  console.log('hello');
 }
 
+//export module for use in 
 module.exports = generateMarkdown;
-
-
-// adding comment for test
